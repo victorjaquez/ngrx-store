c4a0b07587a6c976f96f7778556a7383c078d66a
victorjaquez <victor@jaquez.me>
2019-04-25 16:06:24 -0700

    form logic in component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
 import { addCoinReducer } from './reducers/blockchain.reducer';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BlockchainComponent } from './blockchain/blockchain.component';
@@ -13,7 +14,8 @@ import { BlockchainComponent } from './blockchain/blockchain.component';
   ],
   imports: [
     BrowserModule,
-    StoreModule.forRoot({blockchain: addCoinReducer})
+    StoreModule.forRoot({blockchain: addCoinReducer}),
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/blockchain/blockchain.component.ts
+++ src/app/blockchain/blockchain.component.ts
@@ -1,4 +1,5 @@
-import { BlockChain } from './blockchain.model';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Blockchain } from './blockchain.model';
 import { AppState } from './../app.state';
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
@@ -9,13 +10,22 @@ import { Store } from '@ngrx/store';
   styleUrls: ['./blockchain.component.css']
 })
 export class BlockchainComponent implements OnInit {
+  angForm: FormGroup;
+  constructor(private store: Store<AppState>, private fb: FormBuilder) {
+    this.createForm();
+  }
 
-  constructor(private store: Store<AppState>) { }
+  createForm() {
+    this.angForm = this.fb.group({
+      name: ['', Validators.required],
+      price: ['', Validators.required]
+    });
+  }
 
   addCoin(name, price) {
     this.store.dispatch({
       type: 'ADD_COIN',
-      payload: <BlockChain> {
+      payload: <Blockchain> {
         name: name,
         price: price
       }
