9b818eda952266fa808417fa72e496f4730f1779
victorjaquez <victor@jaquez.me>
2019-04-26 09:18:42 -0700

    removeCoin support

--- src/app/display/display.component.ts
+++ src/app/display/display.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { AppState } from './../app.state';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
-
+import { REMOVE_COIN } from './../reducers/blockchain.reducer';
 @Component({
   selector: 'app-display',
   templateUrl: './display.component.html',
@@ -16,6 +16,15 @@ export class DisplayComponent implements OnInit {
   // subscribing to the store to fetch the data
   constructor(private store: Store<AppState>) {
     this.coins = this.store.select(state => state.blockchain);
+  }
+
+  removeCoin(payload) {
+    if(confirm("Are you sure to delete '" + payload.name + "'?")) {
+      this.store.dispatch({
+        type: REMOVE_COIN,
+        payload: payload
+      });
+    }
   }
 
   ngOnInit() {
--- src/app/reducers/blockchain.reducer.ts
+++ src/app/reducers/blockchain.reducer.ts
@@ -2,6 +2,7 @@ import { Blockchain } from './../blockchain/blockchain.model';
 
 // action type
 export const ADD_COIN = 'ADD_COIN';
+export const REMOVE_COIN = 'REMOVE_COIN';
 
 // reducer function
   // pure function that always returns a new state
@@ -10,6 +11,8 @@ export function addCoinReducer(state: Blockchain[] = [], action) {
   switch (action.type) {
     case ADD_COIN:
       return [...state, action.payload];
+    case REMOVE_COIN:
+      return state.filter(item => item !== action.payload);
     default:
       return state;
   }
